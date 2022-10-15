e31b34cfe02af776de0b352eb84a5850297496db
Kirtan <kachhatrala18@gmail.com>
2022-10-16 01:12:54 +0530

    Middleware and validator in MongoDB

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -7,7 +9,21 @@ const tourSchema = new mongoose.Schema(
             required: [true, 'Tour must have a name'],
             unique: true,
             trim: true,
+            //data validation
+            maxlength: [
+                40,
+                'Tour name must have less or equal then 40 characters',
+            ],
+            minlength: [
+                10,
+                'Tour name must have more or equal then 10 characters',
+            ],
+            // validate: [
+            //     validator.isAlpha,
+            //     'Tour name must only contain characters',
+            // ],
         },
+        slug: String,
         duration: {
             type: Number,
             required: [true, 'Tour must have a duration'],
@@ -19,10 +35,16 @@ const tourSchema = new mongoose.Schema(
         difficulty: {
             type: String,
             required: [true, 'Tour must have a group size'],
+            enum: {
+                values: ['easy', 'medium', 'difficult'],
+                message: 'Difficult is either: easy, medium, difficult',
+            },
         },
         ratingsAverage: {
             type: Number,
             default: 4.5,
+            min: [1, 'Rating must be above 1.0'],
+            max: [5, 'Rating must be below 5.0'],
         },
         ratingsQuantity: {
             type: Number,
@@ -32,7 +54,16 @@ const tourSchema = new mongoose.Schema(
             type: Number,
             required: [true, 'Tour must have a price'],
         },
-        priceDiscount: Number,
+        priceDiscount: {
+            type: Number,
+            validate: {
+                validator: function (val) {
+                    //This only points to current doc on NEW documents creation
+                    return val < this.price * 0.1;
+                },
+                mesage: 'Discount price should be below 10% of the regular price',
+            },
+        },
         summary: {
             type: String,
             trim: true,
@@ -53,6 +84,10 @@ const tourSchema = new mongoose.Schema(
             select: false,
         },
         startDates: [Date],
+        secretTour: {
+            type: Boolean,
+            default: false,
+        },
     },
     {
         toJSON: { virtuals: true },
@@ -62,6 +97,32 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
+});
+
+//Document Middleware: runs before .save() and .create() command not.insertMany()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+//query middleware
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } });
+
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start}`);
+    // console.log(docs);
+    next();
+});
+
+//Aggregation Middleware
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+    next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
