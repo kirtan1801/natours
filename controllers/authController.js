7eb8a5087fd1303bf710ae27e29f855f0ed65d52
Kirtan <kachhatrala18@gmail.com>
2022-10-19 17:23:03 +0530

    Implementing Authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
+const jwt = require('jsonwebtoken');
+const AppError = require('../utils/appError');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+
+const signToken = (id) =>
+    jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
@@ -8,10 +15,36 @@ exports.signup = catchAsync(async (req, res, next) => {
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
     });
+
+    const token = signToken(newUser._id);
+
     res.status(201).json({
         status: 'Success',
+        token,
         data: {
             user: newUser,
         },
+    });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    // 1) check if email and password exist
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password!', 400));
+    }
+
+    // 2) check if user exist and password is correct
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('incorrect email or password', 401));
+    }
+
+    // 3) if everything ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token,
     });
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
-exports.getAllUsers = (req, res) => {
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find();
+    res.status(200).json({
+        status: 'success',
+        result: users.length,
+        data: {
+            users,
+        },
+    });
     res.status(500).json({
         status: 'error',
         message: 'This route is not yet defined...!',
     });
-};
+});
 
 exports.getUser = (req, res) => {
     res.status(500).json({
