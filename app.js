ddc07ccf8e4894f05c7a365fb0094f3bb7332f03
Kirtan <kachhatrala18@gmail.com>
2022-11-15 11:51:11 +0530

    adding data sanitization and preventing parameter polution

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -10,6 +15,11 @@ const userRouter = require('./routers/userRoutes');
 const app = express();
 
 // Global middleware
+
+//Security HTTP Headers
+app.use(helmet());
+
+//development Logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 //Implementing Rate limiter
@@ -20,9 +30,33 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+//Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter polution
+app.use(
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingsQuantity',
+            'ratingsAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price',
+        ],
+    })
+);
+
+//Serving Static file
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
